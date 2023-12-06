e5d5c0fbb423ac39345a61f858c84a87b2f0b25c
Valerie Sanglay <vs242@njit.edu>
2023-12-05 19:26:21 -0500

    Edited: added pages

---
+++ final-project/pages/location.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Location: React.FC = () => {
+    return (
+        <div>
+            <h1>Location & Hours Page</h1>
+            {/* Add your community-related content here */}
+        </div>
+    );
+};
+
+export default Location;
