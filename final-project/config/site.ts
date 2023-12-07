2bdf32780a7419a1dd1e51e2ac307ad04b0457d2
Valerie Sanglay <vs242@njit.edu>
2023-12-06 19:37:48 -0500

    Edited: Navbar component

--- final-project/config/site.ts
+++ final-project/config/site.ts
@@ -51,11 +51,4 @@ export const siteConfig = {
 			href: "/logout",
 		},
 	],
-	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui-docs-v2.vercel.app",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
-	},
 };
--- final-project/pages/index.tsx
+++ final-project/pages/index.tsx
@@ -4,7 +4,6 @@ import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
@@ -21,28 +20,6 @@ export default function IndexPage() {
 					<h4 className={subtitle({ class: "mt-4" })}>
 						Beautiful, fast and modern React UI library.
 					</h4>
-				</div>
-
-				<div className="flex gap-3">
-					<Link
-						isExternal
-						href={siteConfig.links.docs}
-						className={buttonStyles({
-							color: "primary",
-							radius: "full",
-							variant: "shadow",
-						})}
-					>
-						Documentation
-					</Link>
-					<Link
-						isExternal
-						className={buttonStyles({ variant: "bordered", radius: "full" })}
-						href={siteConfig.links.github}
-					>
-						<GithubIcon size={20} />
-						GitHub
-					</Link>
 				</div>
 
 				<div className="mt-8">
