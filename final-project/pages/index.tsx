692a76f977504c5af3416de47bd37603a788330b
Valerie Sanglay <vs242@njit.edu>
2023-12-07 20:12:58 -0500

    Edited; Hero section

--- final-project/pages/index.tsx
+++ final-project/pages/index.tsx
@@ -1,58 +1,58 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
+import style from "../styles/index.module.css";
+// Import your Button component here if you have one
 
 export default function IndexPage() {
-	return (
-		<DefaultLayout>
-			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-				<div className="inline-block max-w-lg text-center justify-center">
-					<h1 className={title()}>Make&nbsp;</h1>
-					<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-					<br />
-					<h1 className={title()}>
-						websites regardless of your design experience.
-					</h1>
-					<h4 className={subtitle({ class: "mt-4" })}>
-						Beautiful, fast and modern React UI library.
-					</h4>
-				</div>
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col justify-center items-center py-8 md:py-10">
+        {/* Top Smaller Rectangle */}
+        <div 
+          style={{ backgroundColor: '#F5A524', height: '48px', width: '100%' }}
+        />
 
-				<div className="flex gap-3">
-					<Link
-						isExternal
-						href={siteConfig.links.docs}
-						className={buttonStyles({
-							color: "primary",
-							radius: "full",
-							variant: "shadow",
-						})}
-					>
-						Documentation
-					</Link>
-					<Link
-						isExternal
-						className={buttonStyles({ variant: "bordered", radius: "full" })}
-						href={siteConfig.links.github}
-					>
-						<GithubIcon size={20} />
-						GitHub
-					</Link>
-				</div>
+        {/* Main Rectangle with Image Overlay */}
+        <div 
+          className="flex justify-center items-center w-full relative" 
+          style={{ backgroundColor: '#312107', height: '565px' }}
+        >
+          {/* Image Overlay */}
+          <div className="absolute top-0 left-0 right-0 bottom-0">
+            <img 
+              src="../images/hero_image.png" 
+              alt="Descriptive Image Text"
+              style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+            />
+          </div>
 
-				<div className="mt-8">
-					<Snippet hideSymbol hideCopyButton variant="bordered">
-						<span>
-							Get started by editing <Code color="primary">pages/index.tsx</Code>
-						</span>
-					</Snippet>
-				</div>
-			</section>
-		</DefaultLayout>
-	);
+          {/* Text and Button Container */}
+          <div className="text-center z-10">
+            <h1 className={style.title}>Welcome to Cottage Comfort Café</h1>
+            <h4 className={style.subtitle}>Where Every Sip Feels Like Home</h4>
+            
+            {/* Buttons */}
+            <div className="mt-4 flex flex-col md:flex-row justify-center items-center gap-4">
+              {/* Join our Family Button */}
+              <button className="bg-warning text-black font-bold py-3 px-4 rounded-lg text-lg" style={{fontSize: 'lg', fontFamily: 'Georgia'}}>
+                Join our Family
+              </button>
+
+              {/* See Menu Button */}
+              <button className="border-4 border-warning text-warning font-bold py-2 px-4 rounded-lg text-lg" style={{fontSize: 'lg', fontFamily: 'Georgia'}}>
+                See Menu
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Bottom Smaller Rectangle */}
+        <div 
+          style={{ backgroundColor: '#F5A524', height: '48px', width: '100%' }}
+        />
+      </section>
+    </DefaultLayout>
+  );
 }
+
+
+
