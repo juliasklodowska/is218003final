7858b30d6ee4e29334e3437a8dab7472a252d79d
Valerie Sanglay <vs242@njit.edu>
2023-12-13 18:58:36 -0500

    Edited: navbar and footer

--- final-project/components/navbar.tsx
+++ final-project/components/navbar.tsx
@@ -1,47 +1,52 @@
+import { useRouter } from 'next/router';
 import {
-	Button,
-	Navbar as NextUINavbar,
-	NavbarBrand,
-  } from "@nextui-org/react";
-  
-  import { siteConfig } from "@/config/site";
-  import NextLink from "next/link";
-  
-  export const Navbar = () => {
-	return (
-	  <NextUINavbar maxWidth="xl" position="sticky" className="font-serif">
-		{/* Brand Logo */}
-		<div className="col-span-2 flex justify-start items-center">
-		  <NextLink href="/" passHref>
-			<NavbarBrand className="font-bold text-inherit">
-			  Cottage Comfort Cafe
-			</NavbarBrand>
-		  </NextLink>
-		</div>
-  
-		{/* Nav Items */}
-		<div className="col-span-8 flex items-center">
-		  {siteConfig.navItems.map((item, index) => (
-			<NextLink key={index} href={item.href} passHref>
-			  <span 
-				className="text-foreground hover:text-primary font-medium cursor-pointer"
-				style={{ marginRight: index === siteConfig.navItems.length - 1 ? 0 : '51px' }}
-			  >
-				{item.label}
-			  </span>
-			</NextLink>
-		  ))}
-		</div>
-  
-		{/* Join Now Button */}
-		<div className="col-span-2 flex justify-end items-center">
-		  <NextLink href="/join" passHref>
-			<Button size="lg" variant="shadow" color="warning">
-			  Join Now
-			</Button>
-		  </NextLink>
-		</div>
-	  </NextUINavbar>
-	);
-  };
+  Button,
+  Navbar as NextUINavbar,
+  NavbarBrand,
+} from "@nextui-org/react";
+import { siteConfig } from "@/config/site";
+import NextLink from "next/link";
+
+export const Navbar = () => {
+  const router = useRouter();
+
+  // Function to determine if the nav item is the current page
+  const isCurrentPage = (href: string) => router.pathname === href;
+
+  return (
+    <NextUINavbar maxWidth="xl" position="sticky" className="font-serif">
+      {/* Brand Logo */}
+      <div className="col-span-2 flex justify-start items-center">
+        <NextLink href="/" passHref>
+          <NavbarBrand className="font-bold text-inherit">
+            Cottage Comfort Cafe
+          </NavbarBrand>
+        </NextLink>
+      </div>
+
+      {/* Nav Items */}
+      <div className="col-span-8 flex items-center">
+        {siteConfig.navItems.map((item, index) => (
+          <NextLink key={index} href={item.href} passHref>
+            <span 
+              className={`text-foreground hover:text-primary font-medium cursor-pointer ${isCurrentPage(item.href) ? 'underline' : ''}`}
+              style={{ marginRight: index === siteConfig.navItems.length - 1 ? 0 : '51px' }}
+            >
+              {item.label}
+            </span>
+          </NextLink>
+        ))}
+      </div>
+
+      {/* Join Now Button */}
+      <div className="col-span-2 flex justify-end items-center">
+        <NextLink href="/join" passHref>
+          <Button size="lg" variant="shadow" color="warning">
+            Join Now
+          </Button>
+        </NextLink>
+      </div>
+    </NextUINavbar>
+  );
+};
 
